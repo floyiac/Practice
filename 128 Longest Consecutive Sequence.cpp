84b3a0a71eada58283574847d04e30fadb33d0b6
floyiac <1554484350@qq.com>
2021-11-14 18:24:14 +0800

    128 Longest Consecutive Sequence

--- 128 Longest Consecutive Sequence.cpp
+++ 128 Longest Consecutive Sequence.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        //创建哈希表 注意unordered_set 中没有重复数字
         unordered_set<int> hashmap;
         for(const int& num : nums){
             hashmap.insert(num);
         }
 
         int ans = 0;
-
+        //注意到连续数字段的首个数字前一位必定没有<key,value> pair 存在
+        //连续数字段中间位置的数前一位都有pair存在
         for(const int& num : hashmap){
             if(!hashmap.count(num-1)){
                 int curr = num;
