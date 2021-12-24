a00d3b0437c1e048b2aa61dad109cd36e5c71381
floyiac <1554484350@qq.com>
2021-12-24 16:46:39 +0800

    Binary Search

---
+++ Binary Search/278 First Bad Version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while(left <= right){
+            int middle  = left + (right - left) / 2;
+            if(isBadVersion(middle)){
+                right = middle - 1;
+            }
+            else{
+                left = middle + 1;
+            }
+        }
+        return left;
+    }
+};
