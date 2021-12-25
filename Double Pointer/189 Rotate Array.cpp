0e5ce88c6798a56191f557ded29ec1dea40bd0d6
floyiac <1554484350@qq.com>
2021-12-26 00:01:45 +0800

    977 Squares of a sorted array

---
+++ Double Pointer/189 Rotate Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    // n is the period
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        int target = k%n;
+        int index;
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            index = (i+target) % n;
+            ans[index] = nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[i] = ans[i];
+        }
+    }
+};
